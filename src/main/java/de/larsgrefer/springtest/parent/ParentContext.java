e7ffacbf6b5a00e530fda50a54d98c35e0758ea1
Lars Grefer <github@larsgrefer.de>
2018-10-16 01:17:56 +0200

    Reproduce on 1.5.16

--- src/main/java/de/larsgrefer/springtest/parent/ParentContext.java
+++ src/main/java/de/larsgrefer/springtest/parent/ParentContext.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
